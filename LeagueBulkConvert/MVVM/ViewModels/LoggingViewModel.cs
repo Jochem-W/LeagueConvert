2fe0837f961d3ffff01fbf3686b45d2fd0d9316c
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2020-10-28 00:55:30 +0100

    Only show the last 256 lines of the log

--- LeagueBulkConvert/MVVM/ViewModels/LoggingViewModel.cs
+++ LeagueBulkConvert/MVVM/ViewModels/LoggingViewModel.cs
@@ -1,33 +1,35 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace LeagueBulkConvert.MVVM.ViewModels
 {
     class LoggingViewModel : INotifyPropertyChanged
     {
-        public bool AutoScroll { get; set; }
-
-        private string log;
-        public string Log
+        public void AddLine(string text, int indent = 0)
         {
-            get => log;
-            set
-            {
-                log = value;
-                OnPropertyChanged();
-            }
+            lines.Add(new string(' ', 4 * indent) + text);
         }
 
-        public void AddLine(string line, int indent = 0)
-        {
-            Log += $"{new string(' ', 4 * indent)}{line}\n";
-        }
+        public bool AutoScroll { get; set; } = true;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly ObservableCollection<string> lines;
+
+        public string Log { get => string.Join('\n', lines.TakeLast(256)); }
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoggingViewModel()
+        {
+            lines = new ObservableCollection<string>();
+            lines.CollectionChanged += (object sender, NotifyCollectionChangedEventArgs e) => OnPropertyChanged("Log");
         }
     }
 }
