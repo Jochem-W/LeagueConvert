30b1f1516d99e03f900fd4c1f7bcf45197fde847
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2020-10-29 22:02:45 +0100

    Move MainWindow code to its ViewModel

---
+++ LeagueBulkConvert/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LeagueBulkConvert
+{
+    class Command : ICommand
+    {
+        private Action<object> action;
+
+        private Func<bool> predicate;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return predicate();
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, null);
+        }
+
+        public Command(Action<object> action, Func<bool> predicate)
+        {
+            this.action = action;
+            this.predicate = predicate;
+        }
+    }
+}
--- LeagueBulkConvert/ViewModels/MainViewModel.cs
+++ LeagueBulkConvert/ViewModels/MainViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System.ComponentModel;
+﻿using LeagueBulkConvert.Views;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace LeagueBulkConvert.ViewModels
 {
     class MainViewModel : INotifyPropertyChanged
     {
+        public ICommand BrowseLeague { get; }
+
+        public ICommand BrowseOutput { get; }
+
+        public Command ConvertCommand { get; }
+
+        public ICommand EditConfigCommand { get; }
+
         private bool allowConversion;
         public bool AllowConversion
         {
@@ -15,6 +29,7 @@ namespace LeagueBulkConvert.ViewModels
             {
                 allowConversion = value;
                 OnPropertyChanged();
+                ConvertCommand.RaiseCanExecuteChanged();
             }
         }
 
@@ -101,13 +116,78 @@ namespace LeagueBulkConvert.ViewModels
             }
         }
 
-        public bool ShowErrors { get; set; }
+        public Window Owner { get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
+        public bool ShowErrors { get; set; }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private string Browse(string initialDirectory)
+        {
+            if (string.IsNullOrWhiteSpace(initialDirectory))
+                initialDirectory = "C:";
+            using var dlg = new CommonOpenFileDialog
+            {
+                InitialDirectory = initialDirectory,
+                IsFolderPicker = true
+            };
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+                return dlg.FileName;
+            return string.Empty;
         }
+
+        private async Task Convert()
+        {
+            AllowConversion = false;
+            LoadingVisibility = Visibility.Visible;
+            var loggingViewModel = new LoggingViewModel();
+            new LoggingWindow(loggingViewModel, Owner).Show();
+            await Task.Run(async () => await Conversion.Converter.StartConversion(this, loggingViewModel));
+            loggingViewModel.AllowSave = true;
+            LoadingVisibility = Visibility.Hidden;
+            AllowConversion = true;
+        }
+
+        private void EditConfig()
+        {
+            var process = new Process { StartInfo = new ProcessStartInfo("config.json") { UseShellExecute = true } };
+            process.Exited += (object sender, EventArgs e) => ((Process)sender).Dispose();
+            try
+            {
+                process.Start();
+            }
+            catch (Exception)
+            {
+                process.Dispose();
+                process = new Process { StartInfo = new ProcessStartInfo("notepad.exe", "config.json") };
+                process.Exited += (object sender, EventArgs e) => ((Process)sender).Dispose();
+                try
+                {
+                    process.Start();
+                }
+                catch (Exception exception)
+                {
+                    process.Dispose();
+                    new MaterialMessageBox(new BoxViewModel
+                    {
+                        Message = $"Couldn't open config.json\n\n{exception.StackTrace}",
+                        Title = "Error"
+                    }).ShowDialog();
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            BrowseLeague = new Command(p => LeaguePath = Browse(LeaguePath), () => true);
+            BrowseOutput = new Command(p => OutPath = Browse(OutPath), () => true);
+            ConvertCommand = new Command(async p => await Convert(), () => AllowConversion);
+            EditConfigCommand = new Command(p => EditConfig(), () => true);
+        }
+
+        public MainViewModel(Window owner) : base() => Owner = owner;
     }
 }
