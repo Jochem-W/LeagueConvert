ca33d7040ce6d8fc251a06d32f41cc4f2e72ea59
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2021-02-08 22:24:53 +0100

    Rewrite League directory check

--- LeagueBulkConvert.WPF/ViewModels/MainPageViewModel.cs
+++ LeagueBulkConvert.WPF/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using LeagueBulkConvert.WPF.Views;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -44,15 +45,22 @@ namespace LeagueBulkConvert.WPF.ViewModels
                 var error = false;
                 try
                 {
-                    var fullPath = Path.GetFullPath(value);
-                    leaguePath = Directory.Exists(Path.Combine(fullPath, "Game", "DATA", "FINAL", "Champions")) ? fullPath : null;
+                    leaguePath = Path.GetFullPath(value);
+                    if (TryGetWadsPath(leaguePath, out string wadsPath))
+                        this.wadsPath = wadsPath;
+                    else
+                    {
+                        leaguePath = null;
+                        this.wadsPath = null;
+                    }
                 }
                 catch (Exception)
                 {
                     leaguePath = null;
+                    wadsPath = null;
                     error = true;
                 }
-                if (leaguePath == null && !error)
+                if ((leaguePath == null || wadsPath == null) && !error)
                     new MessageWindow("Invalid directory",
                         "Please select a valid League of Legends installation directory! (e.g. C:\\Riot Games\\League of Legends)").ShowDialog();
                 OnPropertyChanged();
@@ -64,6 +72,8 @@ namespace LeagueBulkConvert.WPF.ViewModels
         public ICommand NextCommand { get => nextCommand; }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private string wadsPath;
 
         private static string Browse(string initialDirectory)
         {
@@ -77,6 +87,40 @@ namespace LeagueBulkConvert.WPF.ViewModels
 
         private void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
+        private static bool TryGetWadsPath(string path, out string wadsPath)
+        {
+            wadsPath = Path.Combine(path, "Game", "DATA", "FINAL", "Champions");
+            if (Directory.Exists(wadsPath))
+                return true;
+            var downloadPath = Path.Combine(path, "lol_game_client", "releases");
+            var solutionPath = Path.Combine(path, "RADS", "projects", "lol_game_client", "releases");
+            string pathPart;
+            if (Directory.Exists(downloadPath))
+            {
+                pathPart = "files";
+                wadsPath = downloadPath;
+            }
+            else if (Directory.Exists(solutionPath))
+            {
+                pathPart = "deploy";
+                wadsPath = solutionPath;
+            }
+            else
+                return false;
+            IList<string> directories = Directory.GetDirectories(wadsPath);
+            if (directories.Count == 0)
+                return false;
+            var newestVersion = new Version(Path.GetFileName(directories[0]));
+            foreach (var versionString in directories.Skip(1))
+            {
+                var version = new Version(versionString);
+                if (newestVersion.CompareTo(version) < 0)
+                    newestVersion = version;
+            }
+            wadsPath = Path.Combine(wadsPath, newestVersion.ToString(), pathPart, "DATA", "FINAL", "Champions");
+            return true;
+        }
+
         public MainPageViewModel()
         {
             BrowseExport = new Command((_) => ExportPath = Browse(ExportPath));
@@ -89,9 +133,10 @@ namespace LeagueBulkConvert.WPF.ViewModels
                 Directory.CreateDirectory(ExportPath);
                 Directory.SetCurrentDirectory(ExportPath);
                 var config = new Config();
-                foreach (var file in Directory.EnumerateFiles(Path.Combine(LeaguePath, "Game", "DATA", "FINAL", "Champions"), "*.wad.client")
-                    .Where(f => f.Count(c => c == '.') == 2))
-                    config.Wads.Add(new IncludableWad(file));
+                var t = Directory.EnumerateFiles(wadsPath, "*.wad.client");
+                foreach (var filePath in Directory.EnumerateFiles(wadsPath, "*.wad.client")
+                    .Where(f => Path.GetFileName(f).Count(c => c == '.') == 2))
+                    config.Wads.Add(new IncludableWad(filePath));
                 owner.NavigationService.Navigate(new ConfigPage(config));
             }, (_) => LeaguePath != null && ExportPath != null);
         }
